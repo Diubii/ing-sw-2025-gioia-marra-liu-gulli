953342c9366313f82fbe62d48ecaacbab0f79d09
nerd53 <filippogulli83@gmail.com>
2025-03-18 14:40:22 +0100

    Aggiunta metodo Engine per controllare se e' connesso bene (da rimuovere in futuro in caso di implementazione Pattern Visitor )

--- src/main/java/org/polimi/ingsw/galaxytrucker/model/essentials/components/Engine.java
+++ src/main/java/org/polimi/ingsw/galaxytrucker/model/essentials/components/Engine.java
@@ -1,7 +1,12 @@
 package org.polimi.ingsw.galaxytrucker.model.essentials.components;
 
+import org.polimi.ingsw.galaxytrucker.model.Ship;
 import org.polimi.ingsw.galaxytrucker.model.essentials.Component;
+import org.polimi.ingsw.galaxytrucker.model.essentials.Position;
+import org.polimi.ingsw.galaxytrucker.model.essentials.Slot;
 import org.polimi.ingsw.galaxytrucker.model.essentials.Tile;
+
+import java.util.ArrayList;
 
 public class Engine extends Component {
     protected int enginePower;
@@ -20,6 +25,33 @@ public class Engine extends Component {
         if (tempTile.getRotation() != 0) {
             enginePower = 0;
         }
+    }
+
+    public Boolean wellConnected(){
+        Boolean wellConnected = true;
+        Tile tempTile = getMyTile();
+        Ship myShip = tempTile.getMySlot().getMyShip();
+        ArrayList<Position> tempIP = myShip.getInvalidPositions();
+        Slot mySlot = tempTile.getMySlot();
+
+        //posizione nord
+        Position nord = new Position(mySlot.getPosition().getY() -1 , mySlot.getPosition().getX());
+        //posizione sud
+        Position sud = new Position(mySlot.getPosition().getY() +1 , mySlot.getPosition().getX());
+        //posizione est
+        Position est = new Position(mySlot.getPosition().getY() , mySlot.getPosition().getX() + 1);
+        //posizione ovest
+        Position ovest = new Position(mySlot.getPosition().getY() , mySlot.getPosition().getX() -1);
+
+        switch (tempTile.getRotation()){
+            case 0: if (!tempIP.contains(sud) &&  myShip.getShipBoard()[sud.getY()][sud.getX()].getTile() != null ) { wellConnected = false; break;}
+            case 90:if (!tempIP.contains(est) && myShip.getShipBoard()[est.getY()][est.getX()].getTile() != null) { wellConnected = false; break; }
+            case 180:if (!tempIP.contains(nord) && myShip.getShipBoard()[nord.getY()][nord.getX()].getTile() != null) { wellConnected = false; break;}
+            case 270:if (!tempIP.contains(ovest) && myShip.getShipBoard()[ovest.getY()][ovest.getX()].getTile() != null) { wellConnected = false; break;}
+        }
+
+        return wellConnected;
+
     }
 
 
