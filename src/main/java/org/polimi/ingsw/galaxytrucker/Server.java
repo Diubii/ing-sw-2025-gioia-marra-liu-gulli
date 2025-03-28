d9920f5382e92b4c8ecaffc1f641edfa2504bbc6
Diubi <me@diubi.dev>
2025-03-28 14:16:10 +0100

    Client/Server Socket/RMI communication started

--- src/main/java/org/polimi/ingsw/galaxytrucker/Server.java
+++ src/main/java/org/polimi/ingsw/galaxytrucker/Server.java
@@ -1,7 +1,65 @@
 package org.polimi.ingsw.galaxytrucker;
 
-public class Server {
-    public static void main(String[] args) {
+import org.polimi.ingsw.galaxytrucker.model.visitors.AdventureCardActivator;
 
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.MalformedURLException;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+import java.rmi.AlreadyBoundException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Server extends UnicastRemoteObject implements ProvaRMI{
+
+    protected Server() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public String sayHelloRMI() throws RemoteException {
+        return "Hello, RMI";
+    }
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+
+        System.setProperty("java.rmi.server.hostname", "localhost");
+
+        System.out.println("Starting Server...");
+
+        System.out.println("Starting Socket...");
+        try {
+            serverSocket = new ServerSocket(6969);
+            System.out.println("Socket started!");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Could not start Socket.");
+        }
+
+        System.out.println("Starting RMI...");
+        try {
+            LocateRegistry.createRegistry(42069);
+            Naming.rebind("//localhost:42069/prova_rmi", new Server());
+            System.out.println("RMI started!");
+        }
+        catch (RemoteException | MalformedURLException e) {
+            e.printStackTrace();
+            System.err.println("Could not start RMI.");
+        }
+
+        System.out.println("Waiting for socket client...");
+        try{
+            Socket socket = serverSocket.accept();
+        }catch(IOException e){
+            e.printStackTrace();
+            System.err.println("Could not accept client connection.");
+        }
+        System.out.println("Client connected!");
     }
 }
