5fbce8a04221bfbfe85862d198aa6bcf9d1e029c
Mattia <mattia1.marra@mail.polimi.it>
2025-04-22 17:44:52 +0200

    Ripetuta modifica ad alcuni enum per avere fromString

--- src/main/java/org/polimi/ingsw/galaxytrucker/enums/Connector.java
+++ src/main/java/org/polimi/ingsw/galaxytrucker/enums/Connector.java
@@ -1,5 +1,32 @@
 package org.polimi.ingsw.galaxytrucker.enums;
 
 public enum Connector {
-    EMPTY, SINGLE, DOUBLE, UNIVERSAL;
+    EMPTY("Empty"),
+    SINGLE("Single"),
+    DOUBLE("Double"),
+    UNIVERSAL("Universal");
+
+    private final String connectorString;
+
+    // Costruttore per associare una stringa a ciascun valore
+    Connector(String connectorString) {
+        this.connectorString = connectorString;
+    }
+
+    public String getConnectorString() {
+        return connectorString;
+    }
+
+    // Metodo per convertire una stringa nell'enum
+    public static Connector fromString(String text) {
+        if (text != null) {
+            for (Connector connector : Connector.values()) {
+                if (connector.getConnectorString().equalsIgnoreCase(text.trim())) {
+                    return connector;
+                }
+            }
+        }
+        throw new IllegalArgumentException("Nessun valore Connector corrisponde a: " + text);
+    }
+
 }
